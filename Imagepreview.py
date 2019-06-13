287d27866313ae9ae1bd4f9334284d5ba4c0b601
stubbornGarrett <stubborngarrett@gmail.com>
2019-06-13 03:26:43 +0200

    Inital commit

---
+++ Imagepreview.py
@@ -0,0 +1,173 @@
+'''
+***************************************************************
+A tkinter widget, for displaying, moveing and zooming an image.
+***************************************************************
+by stubbornGarrett (GitHub)
+Tested with Python 3.7
+June 2019
+'''
+
+from tkinter import Frame, Canvas
+from PIL     import Image, ImageTk
+
+class Imagepreview(Frame):
+    def __init__(self, parentWidget, image=None, zoomscale=1.2, minzoomlevel=0.1, maxzoomlevel=3, quality=0, backgroundcolor='#999999'):
+        '''
+        A tkinter Frame, which contains a canvas. The canvas adapts to the size of the frame.\n
+        
+        *****Init parameters*****
+        parentWidget:    Widget wich will contain this class (e.g. Tk root window).\n
+        image:           The image, which will be displayed on the canvas (can be changed with '.update_image(*PIL.Image*)').\n
+        backgroundcolor: Color of whitespace on canvas.\n
+        zoomscale:       If the mouse is inside the canvas and the mousewheel gets triggered (Up/Down), the image is scaled by this factor.\n
+        minzoom/maxzoom: Minimum and maximum value, for scaling the image.\n
+        quality (0-4:    Different operations to resize image [NEAREST, BILINEAR, HAMMING, BICUBIC, LANCZOS]
+        *************************
+
+        *********Actions*********
+        To move the image, have the mouse inside the canvas, hold down left mouse button and drag.
+        For zooming, scroll the mouse wheel up or down.
+        *************************
+
+        ***For use under Linux***
+        Sitch the outcommented lines in .creat_binds()
+        *************************
+        '''
+
+        Frame.__init__(self, master=parentWidget)
+        self.previewImage   = image    # Original image
+        self.resizedImage   = None     # Resized image - updates when *canvasScale* changes
+        self.canvasImage    = None     # Displayed image
+        self.canvasImage_ID = None     # Canvas ID of the displayed image
+
+        self.imageLoaded = True if self.previewImage != None else False
+
+        self.canvasScale    = 1.0
+        self.scaleChanged   = True
+
+        self.minlevel       = minzoomlevel
+        self.maxlevel       = maxzoomlevel
+        self.zoomscale      = zoomscale
+
+        self.resizeFilter   = [Image.NEAREST, Image.BILINEAR, Image.HAMMING, Image.BICUBIC, Image.LANCZOS]
+        self.resizeQuality  = quality
+
+        # Create and place canvas
+        self.previewCanvas = Canvas(self, highlightthickness=0, bg=backgroundcolor)
+        self.previewCanvas.grid(column=0, row=0)
+
+        self.create_binds()
+
+        if self.imageLoaded:
+            self.resizeWidth, self.resizeHeight = int(self.previewImage.width*(self.canvasScale)), int(self.previewImage.height*(self.canvasScale))
+            self.resizeSize = self.resizeWidth, self.resizeHeight
+            self.whiteSpaceX = int((self.previewCanvas.winfo_width() -self.resizeWidth) /2)+1
+            self.whiteSpaceY = int((self.previewCanvas.winfo_height()-self.resizeHeight)/2)+1
+
+        self.canvasZeroX = self.previewCanvas.xview()[0]
+        self.canvasZeroY = self.previewCanvas.yview()[0]
+        self.adjust_canvas_size(event=None)
+
+    def create_binds(self):
+        self.bind('<Configure>', self.adjust_canvas_size)
+        self.previewCanvas.bind('<ButtonPress-1>',  self.scroll_start)
+        self.previewCanvas.bind('<B1-Motion>',      self.scroll_move)
+        self.previewCanvas.bind('<MouseWheel>',     self.zoom)
+
+    def adjust_canvas_size(self, event=None):
+        self.previewCanvas.config(width=self.winfo_width(), height=self.winfo_height())
+        self.display_image(image=self.previewImage, quality=self.resizeQuality)
+
+    def scroll_start(self, event):
+        self.previewCanvas.scan_mark(event.x, event.y)
+
+    def scroll_move(self, event):
+        self.previewCanvas.scan_dragto(event.x, event.y, gain=1)
+        if self.check_border_contact():
+            self.display_image(image=self.previewImage, quality=self.resizeQuality)
+
+    def zoom(self, event):
+        if event.delta >= 0 and self.canvasScale*self.zoomscale < self.maxlevel:
+            self.canvasScale *= self.zoomscale
+            self.scaleChanged = True
+        elif event.delta >= 0 and self.canvasScale < self.maxlevel:
+            self.canvasScale = self.maxlevel
+            self.scaleChanged = True
+        if event.delta <  0 and self.canvasScale/self.zoomscale > self.minlevel:
+            self.canvasScale *= 1/self.zoomscale
+            self.scaleChanged = True
+        elif event.delta < 0 and self.canvasScale > self.minlevel:
+            self.canvasScale = self.minlevel
+            self.scaleChanged = True
+        self.display_image(event.x, event.y, self.previewImage, self.resizeQuality)
+
+    def update_image(self, image=None):
+        self.previewImage = image
+        self.resizedImage = image
+        self.imageLoaded = False if self.previewImage == None else True
+        if self.imageLoaded:
+            self.resizeWidth, self.resizeHeight = int(self.previewImage.width*(self.canvasScale)), int(self.previewImage.height*(self.canvasScale))
+            self.resizeSize = self.resizeWidth, self.resizeHeight
+            self.whiteSpaceX = int((self.previewCanvas.winfo_width() -self.resizeWidth) /2)+1
+            self.whiteSpaceY = int((self.previewCanvas.winfo_height()-self.resizeHeight)/2)+1
+
+            self.reset_preview()
+            self.display_image(image=self.previewImage, quality=self.resizeQuality)
+        else:
+            self.previewCanvas.delete(self.previewImage_ID)
+            self.previewImage_ID = None
+
+    def display_image(self, x=0, y=0, image=None, quality=0):
+        if self.imageLoaded:
+            if self.canvasImage_ID:
+                self.previewCanvas.delete(self.canvasImage_ID)
+
+            self.resizeWidth, self.resizeHeight = int(self.previewImage.width*self.canvasScale), int(self.previewImage.height*self.canvasScale)
+            self.resizeSize  = self.resizeWidth, self.resizeHeight
+            self.whiteSpaceX = int((self.previewCanvas.winfo_width() -self.resizeWidth) /2)+1
+            self.whiteSpaceY = int((self.previewCanvas.winfo_height()-self.resizeHeight)/2)+1
+            if self.scaleChanged:
+                self.resizedImage = image.resize(self.resizeSize, resample=self.resizeFilter[quality])
+                self.scaleChanged = False
+
+            leftEdge   = self.previewCanvas.canvasx(0)-self.whiteSpaceX  if self.previewCanvas.canvasx(0) > self.whiteSpaceX  else 0
+            rightEdge  = self.resizeWidth +(self.whiteSpaceX+self.previewCanvas.canvasx(0)) if self.resizeWidth +self.whiteSpaceX-(self.previewCanvas.canvasx(0)) > self.previewCanvas.winfo_width()  else self.resizedImage.width
+            topEdge    = self.previewCanvas.canvasy(0)-self.whiteSpaceY  if self.previewCanvas.canvasy(0) > self.whiteSpaceY  else 0
+            bottomEdge = self.resizeHeight+(self.whiteSpaceY+self.previewCanvas.canvasy(0)) if self.resizeHeight+self.whiteSpaceY-(self.previewCanvas.canvasy(0)) > self.previewCanvas.winfo_height() else self.resizedImage.height
+
+            x = self.previewCanvas.winfo_width() / 2 + (leftEdge + rightEdge  - self.resizeWidth)  /2
+            y = self.previewCanvas.winfo_height()/ 2 + (topEdge  + bottomEdge - self.resizeHeight) /2
+            if rightEdge - leftEdge > 0 and bottomEdge - topEdge > 0:
+                cropedImage         = self.resizedImage.crop((leftEdge, topEdge, rightEdge, bottomEdge))
+                self.canvasImage    = ImageTk.PhotoImage(cropedImage)
+                self.canvasImage_ID = self.previewCanvas.create_image(x, y, image=self.canvasImage)
+
+            # self.previewCanvas.scale('ALL', x, y, self.canvasScale, self.canvasScale)
+
+    def check_border_contact(self):
+        if self.previewCanvas.canvasx(0)+15 > self.whiteSpaceX \
+        or self.resizeWidth +self.whiteSpaceX-(self.previewCanvas.canvasx(0)-15) > self.previewCanvas.winfo_width() \
+        or self.previewCanvas.canvasy(0)+15 > self.whiteSpaceY \
+        or self.resizeHeight+self.whiteSpaceY-(self.previewCanvas.canvasy(0)-15) > self.previewCanvas.winfo_height():
+            print('true')
+            return True
+        else:
+            print('false')
+            return False
+
+    def original_zoom(self, event=None):
+        self.canvasScale  = 1.0
+        self.scaleChanged = True
+        self.display_image(image=self.previewImage, quality=self.resizeQuality)
+
+    def reset_preview(self, event=None):
+        if self.imageLoaded:
+            if self.previewImage.height/self.previewCanvas.winfo_height() > self.previewImage.width/self.previewCanvas.winfo_width():
+                self.canvasScale  = self.previewCanvas.winfo_height() / self.previewImage.height
+                self.scaleChanged = True
+            else:
+                self.canvasScale  = self.previewCanvas.winfo_width()  / self.previewImage.width
+                self.scaleChanged = True
+            self.previewCanvas.xview_moveto(self.canvasZeroX)
+            self.previewCanvas.yview_moveto(self.canvasZeroY)
+            self.adjust_canvas_size()
---
+++ test-klickMeTwice.py
@@ -0,0 +1,134 @@
+from tkinter import Frame, Tk, Label, Button
+from tkinter.filedialog import askopenfilename
+from tkinter.ttk import Separator
+from PIL     import Image, ImageTk
+import Imagepreview
+import time
+
+def main():
+    root = Tk()
+    root.columnconfigure(0, weight=1)
+    root.rowconfigure(0, weight=1)
+    root.geometry('900x600+50+50')
+
+    window = Frame(root)
+    window.grid(column=0, row=0, sticky='news')
+    window.columnconfigure(0, weight=1)
+    window.rowconfigure(0, weight=0)
+    window.rowconfigure(1, weight=1)
+
+    topWidget   = Label(window, text = 'Image Preview', font=('Arial', '32'))
+    topWidget.grid(column=0, row=0, sticky='we')
+    topWidget.columnconfigure(0, weight=1)
+    topWidget.rowconfigure(0, weight=1)
+
+    '''************ImagePreview************'''
+    previewFrame = Frame(window)
+    previewFrame.grid(column=0, row=1, sticky='news')
+    previewFrame.columnconfigure(0, weight=1)
+    previewFrame.rowconfigure(0, weight=1)
+
+    try:
+        testImage = Image.open('test.jpg')
+    except:
+        testImage = Image.new('RGB', (100,100), color='#999999')
+    preview   = Imagepreview.Imagepreview(previewFrame, testImage, zoomscale=1.2, minzoomlevel=0.1, maxzoomlevel=20.0, quality=0, backgroundcolor='#999999')
+    preview.grid(column=0, row=0, sticky='news')
+    '''************************************'''
+
+    buttonColor = 'gray80'
+
+    controlFrame = Frame(window)
+    controlFrame.grid(column=1, row=0, rowspan=3, padx=3)
+    previewFrame.rowconfigure(2, weight=0)
+
+    def update_label(event=None):
+        infoLabel.config(anchor='w', text='Scale:\t\tx{}\n\nWidth:\t\t{}\nHeight:\t\t{}\n\nScale width:\t{}\nScale height:\t{}'.format(round(preview.canvasScale,2), testImage.width, testImage.height, preview.resizedImage.width, preview.resizedImage.height))
+    def reset_button(event=None):
+        preview.reset_preview()
+        update_label()
+    def original_button(event=None):
+        preview.original_zoom()
+        update_label()
+
+    resetButton    = Button(controlFrame, text='Reset Preview (Ctrl+R)', command=reset_button, bg=buttonColor)
+    resetButton.grid(column=0, row=0, sticky='we')
+    originalButton = Button(controlFrame, text='x1.0 zoom (Num 1)', command=original_button, bg=buttonColor)
+    originalButton.grid(column=0, row=1, sticky='we')
+
+    seperatorOne  = Separator(controlFrame)
+    seperatorOne.grid(column=0, row=2, sticky='we', pady=10)
+
+    infoLabel      = Label(controlFrame, anchor='w')
+    infoLabel.grid(column=0, row=3, sticky='s')
+    infoLabel.config(text='Scale:\t\tx{}\n\nWidth:\t\t{}\nHeight:\t\t{}\n\nScale width:\t{}\nScale height:\t{}'.format(round(preview.canvasScale,2), testImage.width, testImage.height, preview.resizedImage.width, preview.resizedImage.height))
+
+    root.bind('<MouseWheel>', update_label)
+    root.bind('1',            original_button)
+    root.bind('<Control-r>',  reset_button)
+
+    def update_quality(x):
+        for child in qualityFrame.winfo_children():
+            try:
+                child.configure(bg='white', fg='black')
+            except:
+                pass
+        if x == 0:
+            qualityZero.configure(bg='gray10', fg='white')
+        elif x == 1:
+            qualityOne.configure(bg='gray10', fg='white')
+        elif x == 2:
+            qualityTwo.configure(bg='gray10', fg='white')
+        elif x == 3:
+            qualityThree.configure(bg='gray10', fg='white')
+        elif x == 4:
+            qualityFour.configure(bg='gray10', fg='white')
+        preview.resizeQuality = x
+        preview.scaleChanged = True
+        preview.display_image(image=preview.previewImage, quality=x)
+
+    seperatorTwo  = Separator(controlFrame)
+    seperatorTwo.grid(column=0, row=4, sticky='we', pady=10)
+
+    qualityFrame  = Frame(controlFrame)
+    qualityFrame.grid(column=0, row=5, sticky='we', padx=30)
+    qualityFrame.columnconfigure(0, weight=1)
+
+    qualityLabel  = Label(qualityFrame, text='Resize Filter', font=('Arial', '10', 'bold'))
+    qualityLabel.grid(column=0, row=0, sticky='we', pady=5)
+    qualityZero   = Button(qualityFrame, text='0 (NEAREST)', anchor='w', command=lambda: update_quality(0))
+    qualityZero.grid(column=0, row=1, sticky='we')
+    qualityOne    = Button(qualityFrame, text='1 (BILINEAR)', anchor='w', command=lambda: update_quality(1))
+    qualityOne.grid(column=0, row=2, sticky='we')
+    qualityTwo    = Button(qualityFrame, text='2 (HAMMING)', anchor='w', command=lambda: update_quality(2))
+    qualityTwo.grid(column=0, row=3, sticky='we')
+    qualityThree  = Button(qualityFrame, text='3 (BICUBIC)', anchor='w', command=lambda: update_quality(3))
+    qualityThree.grid(column=0, row=4, sticky='we')
+    qualityFour   = Button(qualityFrame, text='4 (LANCZOS)', anchor='w', command=lambda: update_quality(4))
+    qualityFour.grid(column=0, row=5, sticky='we')
+    update_quality(0)
+
+    seperatorThree  = Separator(controlFrame)
+    seperatorThree.grid(column=0, row=6, sticky='we', pady=10)
+
+    def load_image():
+        try:
+            filetypes           = [ ('JPEG','*.jpg *.jpeg'),
+                                    ('PNG','*.png'),
+                                    ("all files","*.*")]
+            testImage = Image.open(askopenfilename(title='Load Image...', defaultextension='.', filetypes=filetypes))
+            preview.update_image(image=testImage)
+        except AttributeError:
+            pass
+
+    loadButton = Button(controlFrame, text='Load Image', font=('Arial', '18'),command=load_image, bg=buttonColor)
+    loadButton.grid(column=0, row=7, sticky='we')
+
+    window.update()
+    root.minsize(root.winfo_width(), root.winfo_height())
+    preview.reset_preview()
+    window.mainloop()
+
+
+if __name__ == '__main__':
+    main()
